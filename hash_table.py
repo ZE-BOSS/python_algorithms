afbf28f87fdbc0c68ea05c11f2042c2ad3f1c128
Obi Christopher <ikchristo19@gmail.com>
2023-12-27 11:54:37 +0000

    Hash Table Latest

--- hash_table.py
+++ hash_table.py
@@ -1,25 +1,36 @@
 class HashTable:
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
-    
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
+        
     def get_hash(self, key):
-        h = 0
+        hash = 0
         for char in key:
-            h += ord(char)
-        return h % self.MAX
+            hash += ord(char)
+        return hash % self.MAX
     
-    def __setitem__(self, key, value):
-        h = self.get_hash(key)
-        self.arr[h] = value
-
     def __getitem__(self, key):
+        arr_index = self.get_hash(key)
+        for kv in self.arr[arr_index]:
+            if kv[0] == key:
+                return kv[1]
+            
+    def __setitem__(self, key, val):
         h = self.get_hash(key)
-        return self.arr[h]
-    
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element)==2 and element[0] == key:
+                self.arr[h][idx] = (key,val)
+                found = True
+        if not found:
+            self.arr[h].append((key,val))
+        
     def __delitem__(self, key):
-        h = self.get_hash(key)
-        self.arr[h] = None
+        arr_index = self.get_hash(key)
+        for index, kv in enumerate(self.arr[arr_index]):
+            if kv[0] == key:
+                print("del",index)
+                del self.arr[arr_index][index]
     
 if __name__ == "__main__":
     ht = HashTable()
